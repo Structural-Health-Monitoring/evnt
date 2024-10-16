be87a3db095f1b5224a5935fe444167836949baa
Chrystal Chern <cchern@berkeley.edu>
2024-10-16 01:49:52 -0700

    take list of stations out of script

--- Scripts/NSMP_Buildings.py
+++ Scripts/NSMP_Buildings.py
@@ -19,7 +19,7 @@ print(station_codes)
 
 # 2) Read the data from the `i`th event of `my_station`
 
-my_station = "NP1103"  # set this as the station code of interest
+my_station = "NP1103"  # set this as the station code of interest. see stations_of_interest.md
 i = 0                  # set this as the event number of interest
 
 events = in_dir.glob(f"{my_station}/*p.zip")
@@ -40,27 +40,5 @@ for motion in event_processed.motions.values():
             plt.plot(component.accel.data, label=f"{location} - {direction}")
 plt.legend()
 
-# Some stations of interest:
-# Berkeley; Great Western Savings -- NSMP Station 1103
-# San Francisco; Transamerica Tower -- NSMP Station 1239
-# San Francisco; Chevron Bldg; Structure Array 1 -- NSMP Station 1446
-# Emeryville; Pacific Park Plaza; Structure Array 1 -- NSMP Station 1662
-# CA:SF;New Federal Bld; 1st level -- NSMP Station 1866
-# CA:SF; FDIC; 23rd Fl NW side -- NSMP Station 1876
-# MO:St Louis;One Bell Ctr -- NSMP Station 2490
-# MA:Cambridge;MIT Green Bld 54 -- NSMP Station 2656
-# AK:Anchorage;Hilton Hotel -- NSMP Station 2716
-# Alhambra; LA County Public Works Hdqtrs -- NSMP Station 0482
-# CA Long Beach - VAMC, Bldg 126 -- NSMP Station 5106
-# Los Angeles; 1100 Wilshire -- NSMP Station 5233
-# Los Angeles; UCLA Factor Bldg -- NSMP Station 5405
-# CA:San Diego;US Crthse Annex; 3rd Fl NW -- NSMP Station 5502
-# San Francisco - 62-story Resid. Bldg -- CGS - CSMIP Station 58389
-# WA:Seattle;Crowne Plaza Hotel -- NSMP Station 7010
-# WA:Seattle;New Fed Courthouse -- NSMP Station 7050
-# AK:Anchorage;BP Bld -- NSMP Station 8016
-# Anchorage - R B Atwood Bldg -- NSMP Station 8040
-# Los Angeles; Century City, 2049 CPE -- NSMP Station 0981
-# Los Angeles; Century City, 2029 CPE -- NSMP Station 0982
-# El Castillo Building -- Puerto Rico UPRM Station B03L
+
 
---
+++ Scripts/stations_of_interest.md
@@ -0,0 +1,27 @@
+# Some stations of interest:
+
+| Name                                              | NSMP Station |
+| --------------------------------------------------|--------------|
+| Berkeley; Great Western Savings                   | 1103
+| San Francisco; Transamerica Tower                 | 1239
+| San Francisco; Chevron Bldg; Structure Array 1    | 1446
+| Emeryville; Pacific Park Plaza; Structure Array 1 | 1662
+| CA:SF;New Federal Bld; 1st level                  | 1866
+| CA:SF; FDIC; 23rd Fl NW side                      | 1876
+| MO:St Louis;One Bell Ctr                          | 2490
+| MA:Cambridge;MIT Green Bld 54                     | 2656
+| AK:Anchorage;Hilton Hotel                         | 2716
+| Alhambra; LA County Public Works Hdqtrs           | 0482
+| CA Long Beach - VAMC, Bldg 126                    | 5106
+| Los Angeles; 1100 Wilshire                        | 5233
+| Los Angeles; UCLA Factor Bldg                     | 5405
+| CA:San Diego;US Crthse Annex; 3rd Fl NW           | 5502
+| San Francisco - 62-story Resid. Bldg              | CGS - CSMIP Station 58389
+| WA:Seattle;Crowne Plaza Hotel                     | 7010
+| WA:Seattle;New Fed Courthouse                     | 7050
+| AK:Anchorage;BP Bld                               | 8016
+| Anchorage - R B Atwood Bldg                       | 8040
+| Los Angeles; Century City, 2049 CPE               | 0981
+| Los Angeles; Century City, 2029 CPE               | 0982
+| El Castillo Building                              | Puerto Rico, UPRM Station B03L
+
