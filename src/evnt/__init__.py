dfad57d5bea9e43a07f98a640752daa68f7068fb
Chrystal Chern <cchern@berkeley.edu>
2024-10-30 12:57:22 -0700

    top level global parser

--- src/evnt/__init__.py
+++ src/evnt/__init__.py
@@ -4,37 +4,21 @@
 
 from evnt.core import get_parser
 from pathlib import Path
-from evnt.core import EventRecord, TimeSeries
 
-def read(path_to_zipfile, **kwds) -> EventRecord:
+def read(path_to_zipfile, **kwds):
     """
-    Get the parsed ``record`` from a zip file of event records.
+    Get the `EventRecord` or `TimeSeries` from a file.
 
-    :param path_to_zipfile:     path to zipfile.
+    :param path_to_zipfile:     path to zipfile with extension .zip
+                                or motion file with extension .smc, .v2, etc.
     :type path_to_zipfile:      pathlib Path object, or string.
     
     :return:                    parsed records that are printable (`print`)
-                                and summarizable (`.stats`).
-    :rtype:                     `evnt` EventRecord object.
+                                and summarizable (`.meta`).
+    :rtype:                     if zipfile: `EventRecord`object.
+                                if motion file: `TimeSeries` object.
     """
 
     _, parser = get_parser(path_to_zipfile)
     record = parser(path_to_zipfile,**kwds)
     return record
-
-
-def read_series(path_to_file, **kwds) -> TimeSeries:
-    """
-    Get the parsed ``series`` from a series file.
-
-    :param path_to_file:     path to file.
-    :type path_to_file:      pathlib Path object, or string.
-    
-    :return:                 parsed series that is printable (`print`)
-                             and summarizable (`.stats`).
-    :rtype:                  `evnt` TimeSeries object.
-    """
-
-    _, parser = get_parser(path_to_file)
-    series = parser(path_to_file,**kwds)
-    return series
